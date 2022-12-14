ed178cbf4596ba983063140bd22bfa5ad5db4486
Luka Agic <lukagic@gmail.com>
2022-12-14 17:31:15 +0100

    Practice classes

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
+class Account
+{
+    public int $number;
+    public string $type;
+    public float $balance;
 
+    public function deposit (float $amount): float
+    {
+
+    }
+
+    public function withdraw(float $amount): float
+    {
+
+    }
+
+}
+
+$account = new Account();
+
+$account->balance = 1000.00;
+
+echo $account->balance;
 
 ?>
 <?php require 'includes/header.php'; ?>
