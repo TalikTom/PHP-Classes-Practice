4b704bd334e47ea2c2165d26b7603eb898320ca7
Luka Agic <lukagic@gmail.com>
2022-12-14 18:04:17 +0100

    Add Customer Class

--- index.php
+++ index.php
@@ -17,14 +17,25 @@ class Account
 
 }
 
+class Customer 
+{
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+}
 $account = new Account();
+$customer = new Customer();
+
+$customer->email = 'luka@gmail.com';
 
 $account->balance = 1000.00;
 
-echo $account->balance;
 
 ?>
 <?php require 'includes/header.php'; ?>
 <h1>Hello World</h1>
+<p><?= $account->balance ?></p>
+<p><?= $customer->email; ?></p>
 
 <?php require 'includes/footer.php'; ?>
